647c05e4544e542ab74a32dcbc54eb8b2761daa4
ryanrichardsmith <98512630+ryanrichardsmith@users.noreply.github.com>
2022-02-07 22:07:23 +0400

    Update assignment2.js

--- Feb8/assignment2.js
+++ Feb8/assignment2.js
@@ -3,108 +3,109 @@ function setup() {
   background(220);
 }
 
+//initializing variables used to draw varius fruits
 let xCoordinate = 0;
 let yCoordinate = 0;
 
 function draw() {
-  for (row = 1; row < 5; row = row + 1) {
-      xCoordinate = xCoordinate + 50;
-      yCoordinate = 0;
+  for (row = 1; row < 5; row = row + 1) { //for loop to give each fruit a specific x and y coordinate 
+    xCoordinate = xCoordinate + 50;
+    yCoordinate = 0;
     for (column = 1; column < 4; column = column + 1) {
-      fruit = round(random(1,6));
+      fruit = round(random(1, 6)); //randomly selecting a number to determine which fruit is drawn
       yCoordinate = yCoordinate + 150;
-      if (fruit === 1) {
-        drawGrapes(xCoordinate,yCoordinate);
+      if (fruit === 1) { //conditions assigning each fruit to a number
+        drawGrapes(xCoordinate, yCoordinate);
       } else {
-          if (fruit === 2) {
-          drawWatermelon(xCoordinate,yCoordinate);
+        if (fruit === 2) {
+          drawWatermelon(xCoordinate, yCoordinate);
+        } else {
+          if (fruit === 3) {
+            drawCherries(xCoordinate, yCoordinate);
           } else {
-              if (fruit === 3) {
-                drawCherries(xCoordinate,yCoordinate);
+            if (fruit === 4) {
+              drawOrange(xCoordinate, yCoordinate);
+            } else {
+              if (fruit === 5) {
+                drawApple(xCoordinate, yCoordinate);
               } else {
-                if (fruit === 4) {
-                  drawOrange(xCoordinate,yCoordinate);
-                } else {
-                  if (fruit === 5) {
-                    drawApple(xCoordinate, yCoordinate);
-                  } else {
-                    if (fruit === 6) {
-                      drawLemon(xCoordinate,yCoordinate);
-                    }
-                  }
+                if (fruit === 6) {
+                  drawLemon(xCoordinate, yCoordinate);
                 }
               }
             }
+          }
         }
       }
+    }
   }
 }
 
-function drawGrapes(x,y) {
-  y = y - 40 // to align with other fruits in the row
+function drawGrapes(x, y) {
+  y = y - 40; // to align with other fruits in the row
   let radius = 25;
-  fill(128,0,128);
+  fill(128, 0, 128);
   noStroke();
-  circle(x,y,radius);
+  circle(x, y, radius);
   circle(x - radius, y, radius);
   circle(x + radius, y, radius);
-  circle(x - (1.5*radius), y - radius, radius);
-  circle(x - (radius/2), y - radius, radius);
-  circle(x + (radius/2), y - radius, radius);
-  circle(x + (1.5*radius), y - radius, radius);
-  circle(x - (radius/2), y + radius, radius);
-  circle(x + (radius/2), y + radius, radius);
-  circle(x,y + (2*radius), radius);
+  circle(x - 1.5 * radius, y - radius, radius);
+  circle(x - radius / 2, y - radius, radius);
+  circle(x + radius / 2, y - radius, radius);
+  circle(x + 1.5 * radius, y - radius, radius);
+  circle(x - radius / 2, y + radius, radius);
+  circle(x + radius / 2, y + radius, radius);
+  circle(x, y + 2 * radius, radius);
   fill(139, 69, 19);
-  rect(x-5, y - (2.25*radius), 10, 20);
+  rect(x - 5, y - 2.25 * radius, 10, 20);
 }
 
-function drawWatermelon(x,y) {
+function drawWatermelon(x, y) {
   y = y - 50; //to align with other fruits in the row
-  fill(50,205,50);
+  fill(50, 205, 50);
   noStroke();
-  arc(x,y,150,150,radians(315),radians(135));
-  fill(220,20,60);
-  arc(x,y,120,120,radians(315),radians(135));
+  arc(x, y, 150, 150, radians(315), radians(135));
+  fill(220, 20, 60);
+  arc(x, y, 120, 120, radians(315), radians(135));
 }
 
-function drawCherries(x,y) {
-  fill(255,0,0);
+function drawCherries(x, y) {
+  fill(255, 0, 0);
   noStroke();
   circle(x - 20, y, 50);
   circle(x + 40, y, 50);
   stroke(0);
-  line(x + 35, y - 25, x,y - 70);
+  line(x + 35, y - 25, x, y - 70);
   line(x - 20, y - 25, x, y - 70);
   noStroke();
-  fill(50,205,50);
-  triangle (x, y - 70, x - 10, y - 90, x - 20, y - 90);
+  fill(50, 205, 50);
+  triangle(x, y - 70, x - 10, y - 90, x - 20, y - 90);
 }
 
-function drawOrange(x,y) {
-  y = y - 30 //to align with other fruits in the row
-  fill(255,140,0);
+function drawOrange(x, y) {
+  y = y - 30; //to align with other fruits in the row
+  fill(255, 140, 0);
   noStroke();
-  circle(x,y, 100);
+  circle(x, y, 100);
   fill(139, 69, 19);
   circle(x, y - 40, 10);
 }
 
-function drawApple(x,y) {
-  y = y - 20 //to align with other fruits in the row
-  fill(220,20,60);
+function drawApple(x, y) {
+  y = y - 20; //to align with other fruits in the row
+  fill(220, 20, 60);
   noStroke();
-  circle(x-20,y,100);
-  circle(x + 20,y,100);
+  circle(x - 20, y, 100);
+  circle(x + 20, y, 100);
   fill(139, 69, 19);
-  triangle(x,y-45,x+15,y-60, x+30, y-60);
+  triangle(x, y - 45, x + 15, y - 60, x + 30, y - 60);
 }
 
-function drawLemon(x,y) {
+function drawLemon(x, y) {
   y = y - 20;
-  fill(255,255,0);
-  noStroke()
-  ellipse(x,y,150,100);
-  ellipse(x-75,y,25,25);
-  ellipse(x+75, y,25,25);
+  fill(255, 255, 0);
+  noStroke();
+  ellipse(x, y, 150, 100);
+  ellipse(x - 75, y, 25, 25);
+  ellipse(x + 75, y, 25, 25);
 }
