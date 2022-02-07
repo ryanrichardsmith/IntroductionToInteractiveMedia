a93cde13ebf6f0d85d7929efbe5dde5ac7672f77
ryanrichardsmith <98512630+ryanrichardsmith@users.noreply.github.com>
2022-02-07 13:09:44 +0400

    Update assignment2.js

--- Feb8/assignment2.js
+++ Feb8/assignment2.js
@@ -7,18 +7,20 @@ function draw() {
   drawWatermelon(250,250);
 }
 
+function drawGrapes(x,y) {
+  circle(x - (1.5*radius), y - radius, radius);
+  circle(x + (radius/2), y - radius, radius);
+  circle(x + (1.5*radius), y - radius, radius);
+  circle(x - (radius/2), y + radius, radius);
+  circle(x + (radius/2), y + radius, radius);
+  circle(x,y + (2*radius), radius);
+  fill(139, 69, 19);
+  rect(x-5, y - (2.25*radius), 10, 20);
+}
+
 function drawWatermelon(x,y) {
   fill(50,205,50);
   arc(x,y,150,150,radians(315),radians(135));
   fill(220,20,60);
   arc(x,y,120,120,radians(315),radians(135));
-  for (let seeds = 0; seeds < 11; seeds = seeds + 1) {
-    seedX = random();
-    seedY = random();
-    if (dist(x,y,seedX,seedY) > 120) {
-      seeds = seeds -1;
-    } else {
-      point(seedX, seedY);
-    }
-  }
 }
